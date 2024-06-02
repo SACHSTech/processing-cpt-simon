325d135fc9bfe19096a4557381444cb6742e3909
SimonTse123 <simon.tse25@ycdsbk12.ca>
2024-06-02 19:57:39 -0400

    Animations for Barry the Platypus kicking

--- Sketch1.java
+++ Sketch1.java
@@ -1,37 +1,67 @@
 import processing.core.PApplet;
 import processing.core.PImage;
+
 public class Sketch1 extends PApplet {
-	
-	PImage imgBackground;
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
+  PImage imgBackground;
+  PImage imgPlatypus;
+  PImage[] kickImages;
+
+  boolean isKicking = false;
+  int currentImageIndex = 0;
+  int animationFrameRate = 20;
+
   public void settings() {
-	// put your size call here
     size(750, 750);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
     imgBackground = loadImage("FirstStage.jpg");
     imgBackground.resize(width, height);
+
+    imgPlatypus = loadImage("Barry.png");
+    imgPlatypus.resize(150, 150);
+
+    // Initialize and load kick images
+    kickImages = new PImage[8];
+    for (int i = 0; i < kickImages.length; i++) {
+      kickImages[i] = loadImage("Kick" + (i + 1) + ".png");
+      kickImages[i].resize(150, 150);
+    }
+
+    frameRate(60);
   }
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
   public void draw() {
-	  background(imgBackground);
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
-
-    stroke(255);
-    line(50, 125, 70, 50);  
+    background(imgBackground);
+    
+    if (isKicking) {
+      animateKick();
+    } else {
+      image(imgPlatypus, 550, 550);
+    }
   }
-  
-  // define other methods down here.
+
+  private void animateKick() {
+    image(kickImages[currentImageIndex], 550, 550);
+    if (frameCount % (60 / animationFrameRate) == 0) { // Change frame at the desired rate
+      currentImageIndex = (currentImageIndex + 1) % kickImages.length;
+    }
+  }
+
+  public void keyPressed() {
+    if (key == 'a') {
+      isKicking = true;
+    }
+  }
+
+  public void keyReleased() {
+    if (key == 'a') {
+      isKicking = false;
+      currentImageIndex = 0; 
+    }
+  }
+
+  public static void main(String[] args) {
+    PApplet.main("Sketch1");
+  }
 }
